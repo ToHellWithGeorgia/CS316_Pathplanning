bab5cd4876f45fb4e17cce5357cf6aab0540b825
ToHellWithGeorgia <jackcharm233@gmail.com>
2018-03-16 15:15:41 -0700

    add choose cuda option

--- src_CUDA/PathPlanning.cu
+++ src_CUDA/PathPlanning.cu
@@ -3,7 +3,7 @@
 #include "PathPlanning.h"
 
 /* Number of sample points for valid checker. */
-#define NUMSAMPLE 1024
+#define NUMSAMPLE 1048576
 /* The distance square threshold of being close enough*/
 #define DIST2_THRESHOLD 0.01
 
@@ -114,19 +114,19 @@ validCheckKernel(pp *pp, float x1, float y1, float z1,
                  float stepx, float stepy, float stepz, bool *result)
 {
   int i = blockDim.x * blockIdx.x + threadIdx.x;
-  __shared__ bool temp;
-  temp = true;
-  __syncthreads();
+  // __shared__ bool temp;
+  // / temp = true;
+  // __syncthreads();
 
   ppstate checkState;
   checkState.x = x1 + i * stepx;
   checkState.y = y1 + i * stepy;
   checkState.z = z1 + i * stepz;
   if (!tempValid(&checkState))
-    temp = false;
+    *result = false;
 
-  __syncthreads();
-  *result = temp;
+  // __syncthreads();
+  // *result = temp;
 
 }
 
@@ -154,24 +154,42 @@ isTransitionValid(pp *pp, ppstate *start, ppstate *end)
   float stepy = (y2 - y1) / NUMSAMPLE;
   float stepz = (z2 - z1) / NUMSAMPLE;
 
-  /* Setting up the variable to run CUDA kernel. */ 
-  bool local_res;
-  bool temp = true;
-  bool *result;
-  CUDA_ERR_CK(cudaMalloc((void **)&result, sizeof(bool)));
-  CUDA_ERR_CK(cudaMemcpy(result, &temp, sizeof(bool),
-                         cudaMemcpyHostToDevice));
+  bool use_CUDA = true;
 
-  int numThread = 128;
-  validCheckKernel<<<NUMSAMPLE/numThread, numThread>>>(pp, x1, y1, z1,
-                                                       stepx, stepy, stepz,
-                                                       result);
+  if (use_CUDA)
+  {
+    /* Setting up the variable to run CUDA kernel. */ 
+    bool local_res;
+    bool temp = true;
+    bool *result;
+    CUDA_ERR_CK(cudaMalloc((void **)&result, sizeof(bool)));
+    CUDA_ERR_CK(cudaMemcpy(result, &temp, sizeof(bool),
+                           cudaMemcpyHostToDevice));
 
-  CUDA_ERR_CK(cudaMemcpy(&local_res, result, sizeof(bool),
-                         cudaMemcpyDeviceToHost));
-  CUDA_ERR_CK(cudaFree(result));
+    int numThread = 128;
+    validCheckKernel<<<NUMSAMPLE/numThread, numThread>>>(pp, x1, y1, z1,
+                                                         stepx, stepy, stepz,
+                                                         result);
 
-  return local_res;
+    CUDA_ERR_CK(cudaMemcpy(&local_res, result, sizeof(bool),
+                           cudaMemcpyDeviceToHost));
+    CUDA_ERR_CK(cudaFree(result));
+
+    return local_res;
+  } else
+  {
+    /* TODO: think of ways to parallelize here. */
+    for (uint32_t num = 0; num < NUMSAMPLE; num++)
+    {
+      ppstate checkState;
+      checkState.x = x1 + num * stepx;
+      checkState.y = y1 + num * stepy;
+      checkState.z = z1 + num * stepz;
+      if (!checkValid(pp, &checkState))
+        return false;
+    }
+    return true;
+  }
 }
 
 /* Check if the state is close enough to the goal */
